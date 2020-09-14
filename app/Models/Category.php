d2a92d88af5e89530aa8654a940ad5431759769f
Dreaker <dreaker@beta.arion.com.co-1598091615930-s-1vcpu-2gb-nyc1-01>
2020-09-14 07:42:24 +0000

    Category Model

---
+++ app/Models/Category.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class Category extends Model
+{
+
+    protected $fillable = ['name', 'specs'];
+
+    protected $casts = [
+        'specs' => 'array'
+    ];
+
+    public static function validate(Request $request){
+        $request->validate([
+            "name" => "required"
+        ]);
+    }
+
+    public function getId()
+    {
+        return $this->attributes['id'];
+    }
+
+    public function setId($id)
+
+    {
+        $this->attributes['id'] = $id;
+    }
+
+    public function getName()
+    {
+        return $this->attributes['name'];
+    }
+
+    public function setName($name)
+    {
+        $this->attributes['name'] = $name;
+    }
+
+    public function getSpecs()
+    {
+        return json_decode($this->attributes['specs']);
+    }
+
+    public function setSpecs($specs)
+    {
+        $specs = [];
+
+        foreach ($specs as $spec) {
+            if (!is_null($spec)) {
+                $specs[] = $spec;
+            }
+        }
+    
+        $this->attributes['specs'] = json_encode($specs);
+    }
+}
