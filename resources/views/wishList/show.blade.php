195fa0cfedf13e8b8f129fb8155d44f2b887a2e6
Manuel Gutierrez <magutierrm@eafit.edu.co>
2020-10-14 10:38:12 -0500

    Changes in wish list view

--- resources/views/wishList/show.blade.php
+++ resources/views/wishList/show.blade.php
@@ -2,21 +2,39 @@
 @section("title",$data["title"])
 @section('tab')
 
-<div class="container">
+<div class="wish-list-container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="jumbotron">
-                <h1 class="display-4">{{ __('wishList.title') }}</h1>
+            <div class="jumbotron jumbotron-fluid">
+                <h1 class="display-4" style="text-align:center">{{ __('wishList.title') }}</h1>
+                <hr class="my-4">
                 @foreach($data["products"] as $product)
-                <ul class="list-group">
-                    <li class="list-group-item"> {{ __('wishList.name') }} {{$product->getName()}}</li>
-                    <li class="list-group-item"> {{ __('wishList.price') }} {{$product->getPrice()}}</li>
-                </ul>
-                <form action="{{ route('wishList.deleteProduct', $product->getId() ) }}" method="post">
-                    <input class="btn btn-danger" type="submit" value="Delete" />
-                        @method('delete')
-                        @csrf
-                </form>
+                <div class="row">
+                    <div class="col-sm">
+                        <img src="{{ URL::asset('storage/products/'.$product->getId().'.png') }}" class="img-thumbnail"
+                            alt="...">
+                    </div>
+                    <div class="col-sm">
+                        <ul class="list-group">
+                            <a href="{{route('product.show',$product->getId() ) }}"> <b>{{$product->getName() }}</b> </a>
+                            <li class="list-group-item"> {{ __('wishList.price') }} <b> {{$product->getPrice()}}</b> </li>
+                        </ul>
+                    </div>
+                    <div class="col-sm">
+                        <form action="{{ route('wishList.deleteProduct', $product->getId() ) }}" method="POST">
+                            <button type="submit" class="btn btn-default" aria-label="Left Align">
+                                <span class="fa fa-trash-o fa-lg" aria-hidden="true" style="color:red"></span>
+                            </button>
+                            @csrf
+                        </form>
+                        <form action="{{ route('cart.addToCart',['id'=> $product->getId()]) }}" method="POST">
+                            <button type="submit" class="btn btn-default" aria-label="Left Align">
+                                <span class="fa fa-shopping-cart" aria-hidden="true"></span>
+                            </button>
+                            @csrf
+                        </form>
+                    </div>
+                </div>
                 <hr class="my-4">
                 @endforeach
             </div>
