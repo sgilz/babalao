55fb5198af8837497dad5adba00bfd4b7569d527
sgilz <sgilz@eafit.edu.co>
2020-11-02 21:12:30 -0500

    Implemented product api v1.0

---
+++ app/Http/Controllers/Api/ProductApi.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Http\Resources\ProductCollection;
+use App\Models\Product;
+
+class ProductApi extends Controller
+{
+    public function index()
+    {
+        return new ProductCollection(ProductResource::collection(Product::all()));
+    }
+
+    public function paginate()
+    {
+        return new ProductCollection(ProductResource::collection(Product::paginate(5)));
+    }
+
+    public function show($id)
+    {
+        return new ProductResource(Product::findOrFail($id));
+    }
+}
---
+++ app/Http/Resources/ProductCollection.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    /**
+    * Transform the resource collection into an array.
+    *
+    * @param \Illuminate\Http\Request $request
+    * @return array
+    */
+    public function toArray($request)
+    {
+        return [
+                'collection-data' => [
+                    'id' => 'SKU678743',
+                    'link' => 'http://localhost/laravelcourse/public/product/create',
+                ],
+                'data' => $this->collection,
+        ];
+    }
+}
