64ee9b929d6fa7dad40b5182ce7b653469534247
ManuelG28 <manuelalejandrogutierrezmejia@gmail.com>
2020-11-03 17:55:50 -0500

    Added language controller and fixed issue with user factory

---
+++ resources/lang/es/account.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "header_title" => "Tu cuenta",
+    "side_bar" => [
+        "add_credit_card" => "Añadir tarjeta de credito",
+        "credit_cards" => "Tarjetas de credito",
+        "info" => "Información",
+        "wish_list" => "Lista de deseos",
+    ],
+];
---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Tu contraseña ha sido cambiada!',
+    'sent' => 'Hemos enviado un link a tu correo para que puedas cambiar tu contraseña!',
+    'throttled' => 'Por favor espera mientras lo intentamos.',
+    'token' => 'Este token para cambiar tu contraseña es invalido.',
+    'user' => "No pudimos encontrar este usuario con este email.",
+];
